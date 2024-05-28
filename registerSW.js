423d767b92e34af0db1b49db9db2c34305ad1a7c
Chen Su <chen.su@autodesk.com>
2024-05-28 17:04:57 +0800

    Clear

--- registerSW.js
+++
@@ -1,1 +0,0 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/personal-homepage.github.io/sw.js', { scope: '/personal-homepage.github.io/' })})}
